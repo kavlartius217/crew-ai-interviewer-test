58f8492a1d8141b1642f312aeea3dafeeb39b4bf
kavlartius217 <kavlartius@gmail.com>
2025-02-03 10:58:47 +0530

    Update interviewer.py

--- interviewer.py
+++ interviewer.py
@@ -1,4 +1,6 @@
-import pysqlite3 as sqlite3
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import os
 from crewai import Agent, Task, Crew
